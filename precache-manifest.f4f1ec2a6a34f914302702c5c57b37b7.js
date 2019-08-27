ceca148a5c4b938b9031f24ca9c67322fd9244e4
Yuri Maslennikov <yurij-maslennikov@yandex.ru>
2019-08-27 11:46:14 +0300

    Updates

---
+++ precache-manifest.f4f1ec2a6a34f914302702c5c57b37b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3024be01734d7c81bcc221600cc454ce",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "5b98b3c363dcd51b1255",
+    "url": "/todolist/static/css/main.e61164f8.chunk.css"
+  },
+  {
+    "revision": "4560886a3cb028e7a3be",
+    "url": "/todolist/static/js/2.8a586615.chunk.js"
+  },
+  {
+    "revision": "ba1bae7ce44e9b1630e5",
+    "url": "/todolist/static/js/3.1313ff9a.chunk.js"
+  },
+  {
+    "revision": "5b98b3c363dcd51b1255",
+    "url": "/todolist/static/js/main.db6685b2.chunk.js"
+  },
+  {
+    "revision": "b477430a11809f06bd07",
+    "url": "/todolist/static/js/runtime~main.70b9f007.js"
+  }
+]);
